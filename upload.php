6673886c92b75d7b843733c9e9c476744965c48a
Mixail <98709268+MixailOfficial@users.noreply.github.com>
2022-07-17 21:52:38 +0400

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,6 +1,3 @@
-if(isset($_FILES['file'])){
-   // Переместить загруженный файл в папку со скриптом
-   move_uploaded_file($_FILES['file']['tmp_name'], './' . $_FILES['file']['name']);
-   echo 'Success!';
-}
-else echo 'Error';
+<?php
+file_put_contents('файл.txt',$_GET['text']);
+?>
